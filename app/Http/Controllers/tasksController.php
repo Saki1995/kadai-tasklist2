7d1784515291d96c2c32a7c6a8b072ef75147697
Saki1995 <ec2-user@ip-172-31-39-95.us-east-2.compute.internal>
2018-05-30 07:06:49 +0000

    upload

--- app/Http/Controllers/tasksController.php
+++ app/Http/Controllers/tasksController.php
@@ -46,12 +46,12 @@ class tasksController extends Controller
      */
     public function store(Request $request)
     {    $this->validate($request, [
-            'title' => 'required|max:191',   // 追加
+            'status' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
 
         $task = new Task;
-        $task->title = $request->title;    // 追加
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -97,12 +97,12 @@ class tasksController extends Controller
      */
     public function update(Request $request, $id)
     { $this->validate($request, [
-            'title' => 'required|max:191',   // 追加
+            'status' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
 
         $task = Task::find($id);
-        $task->title = $request->title;    // 追加
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
